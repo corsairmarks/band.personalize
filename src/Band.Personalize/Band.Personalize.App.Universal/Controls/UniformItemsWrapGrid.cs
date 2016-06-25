9dbd5ef491c068cb3b539b41aa3f4dd2cd8f20c3
Nick Butcher <butchern@mail.gvsu.edu>
2016-06-25 00:37:23 -0400

    Uniform (scaling?) wrapping gridfor making tiles

---
+++ src/Band.Personalize/Band.Personalize.App.Universal/Controls/UniformItemsWrapGrid.cs
@@ -0,0 +1,191 @@
+﻿// Copyright 2016 Nicholas Butcher
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Band.Personalize.App.Universal.Controls
+{
+    using System;
+    using Windows.Foundation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// <see cref="ItemsWrapGrid"/>
+    ///
+    /// Positions child elements sequentially from left to right or top to bottom. When
+    /// elements in a row (horizontal) or column (vertical) exceed the maximum, elements
+    /// are positioned in the next row or column. Elements are scaled to fill available
+    /// space assuming there were the specified number of elements in each row or column.
+    /// </summary>
+    /// <remarks>
+    /// The class should flow in the direction of "infinity" for <see cref="ScrollViewer"/>
+    /// attached properties.  It is impossible to divide "infinity" into a collection of equal
+    /// parts for scaling.
+    /// </remarks>
+    public class UniformItemsWrapGrid : Panel
+    {
+        /// <summary>
+        /// Gets the <see cref="MaximumRowsOrColumns"/> dependency property.
+        /// </summary>
+        public static DependencyProperty MaximumRowsOrColumnsProperty { get; } = DependencyProperty.Register("MaximumRowsOrColumns", typeof(int), typeof(UniformItemsWrapGrid), new PropertyMetadata(1, OnMaximumRowsOrColumnsChanged));
+
+        /// <summary>
+        /// Gets the <see cref="Orientation"/> dependency property.
+        /// </summary>
+        public static DependencyProperty OrientationProperty { get; } = DependencyProperty.Register("Orientation", typeof(Orientation), typeof(UniformItemsWrapGrid), null);
+
+        /// <summary>
+        /// Gets or sets a value that influences the wrap point, also accounting for <see cref="Orientation"/>.
+        /// </summary>
+        public int MaximumRowsOrColumns
+        {
+            get { return (int)this.GetValue(MaximumRowsOrColumnsProperty); }
+            set { this.SetValue(MaximumRowsOrColumnsProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the dimension by which child elements are stacked.
+        /// </summary>
+        public Orientation Orientation
+        {
+            get { return (Orientation)this.GetValue(OrientationProperty); }
+            set { this.SetValue(OrientationProperty, value); }
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Measure pass of the layout cycle. Classes can override
+        /// this method to define their own Measure pass behavior.
+        /// </summary>
+        /// <param name="availableSize">
+        /// The available size that this object can give to child objects. Infinity can be
+        /// specified as a value to indicate that the object will size to whatever content
+        /// is available.
+        /// </param>
+        /// <returns>
+        /// The size that this object determines it needs during layout, based on its calculations
+        /// of the allocated sizes for child objects or based on other considerations such
+        /// as a fixed container size.
+        /// </returns>
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            // TODO: deal with "if desired one is infinity"
+            // what if infinity x infinity?
+            double finalWidth, finalHeight;
+            if (this.Orientation == Orientation.Horizontal)
+            {
+                var itemWidth = Math.Floor(availableSize.Width / this.MaximumRowsOrColumns);
+                var tileSize = new Size(itemWidth, itemWidth);
+                finalWidth = availableSize.Width;
+                var actualRows = Math.Ceiling((double)this.Children.Count / this.MaximumRowsOrColumns);
+                var actualHeight = itemWidth * actualRows;
+                foreach (var child in this.Children)
+                {
+                    child.Measure(tileSize);
+                }
+
+                finalHeight = actualHeight;
+            }
+            else
+            {
+                var itemHeight = Math.Floor(availableSize.Height / this.MaximumRowsOrColumns);
+                var tileSize = new Size(itemHeight, itemHeight);
+                finalHeight = availableSize.Height;
+                var actualColumns = Math.Ceiling((double)this.Children.Count / this.MaximumRowsOrColumns);
+                var actualWidth = itemHeight * actualColumns;
+                foreach (var child in this.Children)
+                {
+                    child.Measure(tileSize);
+                }
+
+                finalWidth = itemHeight * actualColumns;
+            }
+
+            return new Size(finalWidth, finalHeight);
+        }
+
+        /// <summary>
+        /// Provides the behavior for the Arrange pass of layout. Classes can override this
+        /// method to define their own Arrange pass behavior.
+        /// </summary>
+        /// <param name="finalSize">
+        /// The final area within the parent that this object should use to arrange itself
+        /// and its children.
+        /// </param>
+        /// <returns>The actual size that is used after the element is arranged in layout.</returns>
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (this.Orientation == Orientation.Horizontal)
+            {
+                var actualRows = Math.Ceiling((double)this.Children.Count / this.MaximumRowsOrColumns);
+                var cellWidth = Math.Floor(finalSize.Width / this.MaximumRowsOrColumns);
+                Size cellSize = new Size(cellWidth, cellWidth);
+                int row = 0, col = 0;
+                foreach (UIElement child in this.Children)
+                {
+                    child.Arrange(new Rect(new Point(cellSize.Width * col, cellSize.Height * row), cellSize));
+                    var element = child as FrameworkElement;
+                    if (element != null)
+                    {
+                        element.Height = cellSize.Height;
+                        element.Width = cellSize.Width;
+                    }
+
+                    if (++col == this.MaximumRowsOrColumns)
+                    {
+                        row++;
+                        col = 0;
+                    }
+                }
+            }
+            else
+            {
+                var actualColumns = Math.Ceiling((double)this.Children.Count / this.MaximumRowsOrColumns);
+                var cellHeight = Math.Floor(finalSize.Height / this.MaximumRowsOrColumns);
+                var cellSize = new Size(cellHeight, cellHeight);
+                int row = 0, col = 0;
+                foreach (UIElement child in this.Children)
+                {
+                    child.Arrange(new Rect(new Point(cellSize.Width * col, cellSize.Height * row), cellSize));
+                    var element = child as FrameworkElement;
+                    if (element != null)
+                    {
+                        element.Height = cellSize.Height;
+                        element.Width = cellSize.Width;
+                    }
+
+                    if (++row == this.MaximumRowsOrColumns)
+                    {
+                        col++;
+                        row = 0;
+                    }
+                }
+            }
+
+            return finalSize;
+        }
+
+        /// <summary>
+        /// The callback that is invoked when the effective property value of the <see cref="MaximumRowsOrColumns"/> property changes, matching <seealso cref="PropertyChangedCallback"/>.
+        /// </summary>
+        /// <param name="obj">The <see cref="DependencyObject"/> on which the property has changed value.</param>
+        /// <param name="e">Event data that is issued by any event that tracks changes to the effective value of this property.</param>
+        private static void OnMaximumRowsOrColumnsChanged(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            int cols = (int)e.NewValue;
+            if (cols < 1)
+            {
+                ((UniformItemsWrapGrid)obj).MaximumRowsOrColumns = 1;
+            }
+        }
+    }
+}
